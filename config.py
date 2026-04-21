ce040377850a24846c9a72b901d1f293a8ffcfc1
vazir0028 <9403923533pk@gmail.com>
2026-04-21 14:13:36 +0530

    Add configuration variables for API integration

---
+++ config.py
@@ -0,0 +1,4 @@
+CLIENT_ID = "YOUR_CLIENT_ID"
+CLIENT_SECRET = "YOUR_CLIENT_SECRET"
+REDIRECT_URI = "http://localhost:8888/callback"
+SCOPE = "user-read-playback-state,user-modify-playback-state,user-read-currently-playing"
